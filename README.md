76bcf603570f18595365329c25ef94b9a9502b8c
Constantin Bogza <tinel.c@gmail.com>
2024-10-27 09:57:24 +0200

    Initial modifications after running on linux

--- README.md
+++ README.md
@@ -26,15 +26,35 @@ sudo apt install python3-pip
 '''
 
 Install python packages
+
 '''
+python3 -m venv .venv
+source .venv/bin/activate
+python3 -m pip install -r requirements.txt
 pip install selenium
 pip install beautifulsoup4
 pip install lxml
 pip install paho-mqtt
+pip install webdriver-manager
 '''
 
 '''
 sudo apt install python3-selenium
 sudo apt-get install python3-bs4
 sudo apt install python3-paho-mqtt
-''
+'''
+Create directory for the python script
+'''
+mkdir pyFetchHuaweiData
+cd pyFetchHuaweiData
+'''
+
+Clone github respository
+'''
+git clone https://github.com/tinel-c/Huawei_Inverter_Kiosk_Data_Parser.git
+'''
+
+Get Chrome driver 
+https://skolo.online/documents/webscrapping/#step-2-install-chromedriver
+
+
--- huawei_kiosk_parser.py
+++ huawei_kiosk_parser.py
@@ -3,42 +3,55 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
 from time import gmtime, strftime
+#from webdriver_manager.chrome import ChromeDriverManager
 import time
 import datetime
 import paho.mqtt.client as mqtt
+from selenium.webdriver.chrome.service import Service
+import paho.mqtt.publish as publish
+
 
 # The callback for when the client receives a CONNACK response from the server.
-def on_connect(client, userdata, flags, reason_code, properties):
-    print(f"Connected with result code {reason_code}")
+#def on_connect(client, userdata, flags, reason_code, properties):
+#    print(f"Connected with result code {reason_code}")
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    client.subscribe("$SYS/#")
+#    client.subscribe("$SYS/#")
 
 # The callback for when a PUBLISH message is received from the server.
-def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
+#def on_message(client, userdata, msg):
+#    print(msg.topic+" "+str(msg.payload))
 
-mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
-mqttc.on_connect = on_connect
-mqttc.on_message = on_message
+#mqttc = mqtt.Client()
+#mqttc.on_connect = on_connect
+#mqttc.on_message = on_message
 
-mqttc.connect("192.168.2.4", 1883, 60)
+#mqttc.connect("192.168.2.4", 1883, 60)
 
 
 #Get data from the webpage
 
 #--| Setup
+#service = Service(executable_path=r'/usr/bin/chromedriver')
 options = Options()
+options.add_argument('--headless')
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+options.add_argument("--remote-debugging-port=9222")
+options.headless = True
+#options.headless = True
 #options.add_argument("--headless")
 #options.add_argument("--window-size=1980,1020")
+#browser = webdriver.Chrome(service=service, options=options)
 browser = webdriver.Chrome(options=options)
+#browser = webdriver.Chrome(service=ChromeDriverManager().install(),options=options)
 #--| Parse or automation
 url = "https://uni003eu5.fusionsolar.huawei.com/pvmswebsite/nologin/assets/build/cloud.html#/kiosk?kk=8DkvnsA41e3fbzMJwPFe1OFKpcwE9Aqr"
 browser.get(url)
 time.sleep(3)
 # Use BeautifulSoup
 soup = BeautifulSoup(browser.page_source, 'lxml')
-print(soup)
+#print(soup)
 title = soup.find_all('p', class_="nco-kiosk-overview-data")
 
 print(title[0].text)
@@ -49,17 +62,36 @@ print(title[4].text)
 now = datetime.datetime.now()
 print(now)
 
-mqttc.publish("Huawei/real-power", title[0].text[:-2])
-mqttc.publish("Huawei/real-power-unit", title[0].text[-2:])
-mqttc.publish("Huawei/yield-today", title[1].text[:-3])
-mqttc.publish("Huawei/yield-today-unit", title[1].text[-3:])
-mqttc.publish("Huawei/yield-this-month", title[2].text[:-3])
-mqttc.publish("Huawei/yield-this-month-unit", title[2].text[-3:])
-mqttc.publish("Huawei/yield-this-year", title[3].text[:-3])
-mqttc.publish("Huawei/yield-this-year-unit", title[3].text[-3:])
-mqttc.publish("Huawei/total-yield", title[4].text[:-3])
-mqttc.publish("Huawei/total-yield-unit", title[4].text[-3:])
-mqttc.publish("Huawei/report-time", str(now))
+#mqttc.publish("Huawei/real-power", title[0].text[:-2])
+#mqttc.publish("Huawei/real-power-unit", title[0].text[-2:])
+#mqttc.publish("Huawei/yield-today", title[1].text[:-3])
+#mqttc.publish("Huawei/yield-today-unit", title[1].text[-3:])
+#mqttc.publish("Huawei/yield-this-month", title[2].text[:-3])
+#mqttc.publish("Huawei/yield-this-month-unit", title[2].text[-3:])
+#mqttc.publish("Huawei/yield-this-year", title[3].text[:-3])
+#mqttc.publish("Huawei/yield-this-year-unit", title[3].text[-3:])
+#mqttc.publish("Huawei/total-yield", title[4].text[:-3])
+#mqttc.publish("Huawei/total-yield-unit", title[4].text[-3:])
+#mqttc.publish("Huawei/report-time", str(now))
+
+publish.single("Huawei/real-power", payload=title[0].text[:-2],hostname="192.168.2.4",port=1883, client_id="huawei_parser")
+publish.single("Huawei/real-power-unit", payload=title[0].text[-2:],hostname="192.168.2.4",port=1883, client_id="huawei_parser")
+
+publish.single("Huawei/yield-today", payload=title[1].text[:-3],hostname="192.168.2.4",port=1883, client_id="huawei_parser")
+publish.single("Huawei/yield-today-unit", payload=title[1].text[-3:],hostname="192.168.2.4",port=1883, client_id="huawei_parser")
+
+publish.single("Huawei/yield-this-month", payload=title[2].text[:-3],hostname="192.168.2.4",port=1883, client_id="huawei_parser")
+publish.single("Huawei/yield-this-month-unit", payload=title[2].text[-3:],hostname="192.168.2.4",port=1883, client_id="huawei_parser")
+
+publish.single("Huawei/yield-this-year", payload=title[3].text[:-3],hostname="192.168.2.4",port=1883, client_id="huawei_parser")
+publish.single("Huawei/yield-this-year-unit", payload=title[3].text[-3:],hostname="192.168.2.4",port=1883, client_id="huawei_parser")
+
+publish.single("Huawei/total-yield", payload=title[4].text[:-3],hostname="192.168.2.4",port=1883, client_id="huawei_parser")
+publish.single("Huawei/total-yield-unit", payload=title[4].text[-3:],hostname="192.168.2.4",port=1883, client_id="huawei_parser")
+
+publish.single("Huawei/report-time", payload=str(now),hostname="192.168.2.4",port=1883, client_id="huawei_parser")
+
+
 
 
 
@@ -68,3 +100,4 @@ mqttc.publish("Huawei/report-time", str(now))
 # Other loop*() functions are available that give a threaded interface and a
 # manual interface.
 #mqttc.loop_forever()
+
